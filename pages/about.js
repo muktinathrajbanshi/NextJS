54fa037a04cc61aba4d285891dd5b2de051741d3
unknown <muktinathrajbanshi786@gmail.com>
2025-08-25 16:03:25 +0545

    adding multiple pages in NextJS app

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <>
+      <h2>Hello this is about page</h2>
+    </>
+  );
+};
+
+export default about;
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <>
+      <h1>hello this is contact page</h1>
+    </>
+  );
+};
+
+export default contact
