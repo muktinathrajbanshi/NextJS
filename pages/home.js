291acc6b35bed83d6b8cb757105cb8169a0d4683
unknown <muktinathrajbanshi786@gmail.com>
2025-08-25 16:00:16 +0545

    playing with version for working my application

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+
+const home = () => {
+  return (
+    <>
+      <h1>Hello this is home page</h1>
+    </>
+  );
+};
+
+export default home;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+      <h1>Hello index page</h1>
+    </>
+  )
+}
+
+export default index;
