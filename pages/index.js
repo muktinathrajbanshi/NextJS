5bda9a701ef0d6acff0fb1a52929e528beb968d2
unknown <muktinathrajbanshi786@gmail.com>
2025-08-25 17:46:46 +0545

    Link Component | Create Navbar without Page Refresh

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import Link from "next/link";
 
 const index = () => {
   return (
     <>
-      <h1>Hello index page</h1>
+      <nav className="menu-list">
+        <ul className="menu-bar">
+           <li>
+             <Link href="/">
+                <a>Home</a>
+            </Link>
+           </li>
+            <li>
+            <Link href="/about">
+             <a>About</a>
+            </Link>
+            </li>
+           <li>
+             <Link  href="/contact">
+                <a>Contact</a>
+            </Link>
+           </li>
+           <li>
+             <Link a href="/blog">
+                <a>Blog</a>
+            </Link>
+           </li>
+           <li>
+             <Link href="/product">
+                <a>Product</a>
+            </Link>
+           </li>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
 
 export default index;
---
+++ pages/product.js
@@ -0,0 +1,10 @@
+
+const product = () => {
+  return (
+    <>
+      <h1>Hello this is product page</h1>
+    </>
+  );
+};
+
+export default product
