09b2266e511e58663ebdae890643682ddf53ebfb
unknown <muktinathrajbanshi786@gmail.com>
2025-08-25 17:03:11 +0545

    Next.JS Nested File Based Routing

---
+++ pages/blog/blog1.js
@@ -0,0 +1,10 @@
+
+const blog1 = () => {
+  return (
+    <>
+      <h1>Hello this is blog 1 page</h1>
+    </>
+  );
+};
+
+export default blog1;
---
+++ pages/blog/blog2.js
@@ -0,0 +1,10 @@
+
+const blog2 = () => {
+  return (
+    <>
+      <h1>Hello this is blog 2 page</h1>
+    </>
+  );
+};
+
+export default blog2;
