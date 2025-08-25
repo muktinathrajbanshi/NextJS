e28ad611148dd432bd2bdb24e59aa0d34a925472
unknown <muktinathrajbanshi786@gmail.com>
2025-08-25 21:43:07 +0545

    Creating our Own Next.JS 404 Page

---
+++ pages/404.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+const Errorpage = () => {
+  return (
+    <>
+        <nav className="menu-list">
+        <ul className="menu-bar">
+           <li>
+             <Link href="/">
+                <a>Home</a>
+            </Link>
+           </li>
+            <li>
+            <Link href="/about">
+             <a>About</a>
+            </Link>
+            </li>
+           <li>
+             <Link  href="/contact">
+                <a>Contact</a>
+            </Link>
+           </li>
+           <li>
+             <Link a href="/blog">
+                <a>Blog</a>
+            </Link>
+           </li>
+           <li>
+             <Link href="/product">
+                <a>Product</a>
+            </Link>
+           </li>
+        </ul>
+      </nav>
+
+      <div id="notfound">
+        <div className="notfound">
+            <div className="notfound-404">
+                <h1>404</h1>
+            </div>
+            <h2>We are sorry, Page not found!</h2>
+            <p>
+                The page you are looking for might have been removed had its class 
+                changed or is temporarily unavailable.
+            </p>
+            <a href="/" className="anchor-home">Back to Homepage</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Errorpage
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,8 @@
-import React from 'react'
 
 const about = () => {
   return (
     <>
-      <h2>Hello this is about page</h2>
+      <h1>Hello this is about page</h1>
     </>
   );
 };
