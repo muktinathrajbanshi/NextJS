8492c1b05470da345bf980e562b944e749f877e0
unknown <muktinathrajbanshi786@gmail.com>
2025-08-27 22:52:20 +0545

    Rest API | Next.JS GetStaticProps() for Data Fetching

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,31 @@
 import Navbar from "../../components/Navbar";
-const Blog = () => {
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+
+  return {
+    props: {
+      data,
+    },
+  };
+
+};
+
+const Blog = ({data}) => {
   return (
     <>
       <Navbar />
-     <h1>Welcome to blog page</h1> 
+      {
+        data.map((curElem) => {
+          return <div key={curElem.id}>
+              <h3>{curElem.id}</h3>
+              <h2>{curElem.title}</h2>
+          </div>
+        })
+      }
     </>
   );
 };
