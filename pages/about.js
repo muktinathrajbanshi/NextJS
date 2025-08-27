a4bef8577c76473216587de004708ca2b1feb215
unknown <muktinathrajbanshi786@gmail.com>
2025-08-27 19:06:36 +0545

    adding head tag inside about page also

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import Navbar from "../components/Navbar";
 import styles from "../styles/about.module.css";
 import Image from "next/image";
+import Head from "next/head";
 const about = () => {
   return (
     <>
+      <Head>
+      <title>About Page</title>
+      </Head>
       <Navbar />
       <div style={{textAlign: "center"}}>
       <h1 className={styles.main_heading_h}>Hello this is about page</h1>
