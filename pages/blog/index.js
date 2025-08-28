d7ef8c66a15275e67cf456179962367364db2c91
unknown <muktinathrajbanshi786@gmail.com>
2025-08-28 13:19:13 +0545

    design is created for better visibility

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,5 @@
 import Navbar from "../../components/Navbar";
-
+import styles from "../../styles/blog.module.css";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -20,9 +20,9 @@ const Blog = ({data}) => {
       <Navbar />
       {
         data.slice(0, 5).map((curElem) => {
-          return <div key={curElem.id}>
-              <h1>{curElem.id}</h1>
-              <h1>{curElem.title}</h1>
+          return <div key={curElem.id} className={styles.blog_style}>
+              <h3 className={styles.heading_one}>{curElem.id}</h3>
+              <h4 className={styles.heading_two}>{curElem.title}</h4>
           </div>
         })
       }
