fb33cfca9ec7f57f05b7923e64cb356a924de20d
unknown <muktinathrajbanshi786@gmail.com>
2025-08-28 14:39:41 +0545

    Next.JS GetStaticPaths() for Rest API & Dynamic Route

--- pages/blog/[pageNo].js
+++ pages/blog/[pageNo].js
@@ -1,13 +1,49 @@
-import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar";
+import styles from "../../styles/blog.module.css";
 
-const pageNo = () => {
-    const router = useRouter();
-    const pageNumber = router.query.pageNo;
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const paths = data.map((curElem) => {
+    return {
+      params: {
+        pageNo: curElem.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pageNo;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  
+  return {
+    props: {
+      data,
+    },
+  };
+
+};
+
+const myData = ({ data }) => {
+  const {id, title, body} = data;
   return (
     <>
-      <h1>Hello this is blog {pageNumber} page</h1>
+    <Navbar />
+    <div className={styles.blog_style_inside}>
+      <h3 className={styles.heading_one_ok}>{id}</h3>
+        <h4 className={styles.heading_two_ok}>{title}</h4>
+        <p className={styles.para_ok}>{body}</p>
+    </div>
     </>
   );
 };
 
-export default pageNo;
+export default myData;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,11 @@
 import Navbar from "../../components/Navbar";
 import styles from "../../styles/blog.module.css";
+import Link from "next/link";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
-
-
+  
   return {
     props: {
       data,
@@ -22,7 +22,9 @@ const Blog = ({data}) => {
         data.slice(0, 5).map((curElem) => {
           return <div key={curElem.id} className={styles.blog_style}>
               <h3 className={styles.heading_one}>{curElem.id}</h3>
+              <Link href={`/blog/${curElem.id}`}>
               <h4 className={styles.heading_two}>{curElem.title}</h4>
+              </Link>
           </div>
         })
       }
